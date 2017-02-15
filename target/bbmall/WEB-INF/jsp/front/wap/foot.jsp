780f430386da29c05a1ba61dffca18ebf7354d1b
qifaqiang <qifaqiang1990@163.com>
2017-02-15 10:45:57 +0800

    Signed-off-by: qifaqiang <qifaqiang1990@163.com>

--- target/bbmall/WEB-INF/jsp/front/wap/foot.jsp
+++
@@ -1,4 +0,0 @@
-<%@ page language="java" import="java.util.*" pageEncoding="utf-8"%>
-<script src="${SHOPDOMAIN}/front/js/jquery.hDialog.js"></script>
-</body>
-</html>
--- target/bbmall/front/js/wap/checkLogin.js
+++
@@ -1,5 +0,0 @@
-if (userId == "") {
-	window.location.href = "login.html";
-}else{
-	document.addEventListener('WeixinJSBridgeReady', function onBridgeReady() { WeixinJSBridge.call('hideOptionMenu'); });
-}
